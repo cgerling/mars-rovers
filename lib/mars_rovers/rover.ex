132bfd85e88a7a3528a90627b6ddcc45f4e3119a
Calvin Gerling <calvin@gerling.sh>
2021-06-04 22:25:57 -0300

    create function to change a rover's heading direction

--- lib/mars_rovers/rover.ex
+++ lib/mars_rovers/rover.ex
@@ -4,11 +4,29 @@ defmodule MarsRovers.Rover do
   @enforce_keys [:heading, :x, :y]
   defstruct [:heading, :x, :y]
 
-  alias MarsRovers.CardinalPoint
+  alias MarsRovers.{CardinalPoint, Direction}
 
   @type t :: %__MODULE__{
           heading: CardinalPoint.t(),
           x: integer(),
           y: integer()
         }
+
+  @left Direction.left()
+  @right Direction.right()
+
+  @spec turn(%__MODULE__{}, Direction.t()) :: %__MODULE__{}
+  def turn(%__MODULE__{} = rover, direction) do
+    direction_ordinal = convert_direction_to_ordinal(direction)
+
+    heading =
+      rover.heading
+      |> CardinalPoint.get_directions_for()
+      |> elem(direction_ordinal)
+
+    %{rover | heading: heading}
+  end
+
+  defp convert_direction_to_ordinal(@left), do: 0
+  defp convert_direction_to_ordinal(@right), do: 1
 end
--- test/mars_rovers/rover_test.exs
+++ test/mars_rovers/rover_test.exs
@@ -1,3 +1,92 @@
 defmodule MarsRovers.RoverTest do
   use ExUnit.Case, async: true
+
+  alias MarsRovers.{CardinalPoint, Rover}
+
+  @east CardinalPoint.east()
+  @north CardinalPoint.north()
+  @south CardinalPoint.south()
+  @west CardinalPoint.west()
+
+  describe "turn/2" do
+    test "returns a rover heading to west when rover was faced north and turned to left" do
+      rover = %Rover{x: 5, y: 5, heading: @north}
+
+      turned_rover = Rover.turn(rover, :left)
+
+      assert turned_rover.heading == @west
+      assert turned_rover.x == rover.x
+      assert turned_rover.y == rover.y
+    end
+
+    test "returns a rover heading to east when rover was faced north and turned to right" do
+      rover = %Rover{x: 5, y: 5, heading: @north}
+
+      turned_rover = Rover.turn(rover, :right)
+
+      assert turned_rover.heading == @east
+      assert turned_rover.x == rover.x
+      assert turned_rover.y == rover.y
+    end
+
+    test "returns a rover heading to north when rover was faced east and turned to left" do
+      rover = %Rover{x: 5, y: 5, heading: @east}
+
+      turned_rover = Rover.turn(rover, :left)
+
+      assert turned_rover.heading == @north
+      assert turned_rover.x == rover.x
+      assert turned_rover.y == rover.y
+    end
+
+    test "returns a rover heading to south when rover was faced east and turned to right" do
+      rover = %Rover{x: 5, y: 5, heading: @east}
+
+      turned_rover = Rover.turn(rover, :right)
+
+      assert turned_rover.heading == @south
+      assert turned_rover.x == rover.x
+      assert turned_rover.y == rover.y
+    end
+
+    test "returns a rover heading to east when rover was faced south and turned to left" do
+      rover = %Rover{x: 5, y: 5, heading: @south}
+
+      turned_rover = Rover.turn(rover, :left)
+
+      assert turned_rover.heading == @east
+      assert turned_rover.x == rover.x
+      assert turned_rover.y == rover.y
+    end
+
+    test "returns a rover heading to west when rover was faced south and turned to right" do
+      rover = %Rover{x: 5, y: 5, heading: @south}
+
+      turned_rover = Rover.turn(rover, :right)
+
+      assert turned_rover.heading == @west
+      assert turned_rover.x == rover.x
+      assert turned_rover.y == rover.y
+    end
+
+    test "returns a rover heading to south when rover was faced west and turned to left" do
+      rover = %Rover{x: 5, y: 5, heading: @west}
+
+      turned_rover = Rover.turn(rover, :left)
+
+      assert turned_rover.heading == @south
+      assert turned_rover.x == rover.x
+      assert turned_rover.y == rover.y
+    end
+
+    test "returns a rover heading to north when rover was faced west and turned to right" do
+      rover = %Rover{x: 5, y: 5, heading: @west}
+
+      turned_rover = Rover.turn(rover, :right)
+
+      assert turned_rover.heading == @north
+      assert turned_rover.x == rover.x
+      assert turned_rover.y == rover.y
+    end
+  end
 end
