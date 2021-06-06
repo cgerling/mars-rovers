4e1326ec7becec384332ff1237e8d64f5ecdb07a
Calvin Gerling <calvin@gerling.sh>
2021-06-06 20:01:01 -0300

    create function to deploy a squad of rovers with instructions

--- lib/mars_rovers.ex
+++ lib/mars_rovers.ex
@@ -1,3 +1,14 @@
 defmodule MarsRovers do
   @moduledoc false
+
+  alias MarsRovers.{Commander, Parser, Rover}
+
+  def deploy(deploy_instructions) do
+    {_, rovers_with_commands} = Parser.parse_deploy_instructions(deploy_instructions)
+
+    rovers_with_commands
+    |> Enum.map(fn {%Rover{} = rover, commands} -> Commander.command_rover(rover, commands) end)
+    |> Enum.map(&Rover.to_string/1)
+    |> Enum.join("\n")
+  end
 end
--- test/mars_rovers_test.exs
+++ test/mars_rovers_test.exs
@@ -1,3 +1,31 @@
 defmodule MarsRoversTest do
   use ExUnit.Case
+
+  describe "deploy/1" do
+    test "returns a string with the rover's final position with coordinates and heading direction" do
+      deploy_instructions = """
+      5 5
+      1 2 N
+      LMLMLMLMM
+      """
+
+      rover_position = "1 3 N"
+
+      assert rover_position == MarsRovers.deploy(deploy_instructions)
+    end
+
+    test "returns a multiline string with the final position of each rover in the same order of the instructions" do
+      deploy_instructions = """
+      5 5
+      1 2 N
+      LMLMLMLMM
+      3 3 E
+      MMRMMRMRRM
+      """
+
+      rovers_position = "1 3 N\n5 1 E"
+
+      assert rovers_position == MarsRovers.deploy(deploy_instructions)
+    end
+  end
 end
