6dbd454e9dde3a2f2ab39a51742f5f5fe99e5c90
Calvin Gerling <calvin@gerling.sh>
2021-06-08 14:05:41 -0300

    use atoms directly instead of module attributes

--- lib/mars_rovers/cardinal_point.ex
+++ lib/mars_rovers/cardinal_point.ex
@@ -3,48 +3,38 @@ defmodule MarsRovers.CardinalPoint do
 
   alias MarsRovers.Direction
 
-  @type east :: :east
-  @type north :: :north
-  @type south :: :south
-  @type west :: :west
-
-  @type t :: east() | north() | south() | west()
-
-  @east :east
-  @north :north
-  @south :south
-  @west :west
+  @type t :: :east | :north | :south | :west
 
   @left Direction.left()
   @right Direction.right()
 
-  @spec east() :: east()
-  def east, do: @east
+  @spec east() :: :east
+  def east, do: :east
 
-  @spec north() :: north()
-  def north, do: @north
+  @spec north() :: :north
+  def north, do: :north
 
-  @spec south() :: south()
-  def south, do: @south
+  @spec south() :: :south
+  def south, do: :south
 
-  @spec west() :: west()
-  def west, do: @west
+  @spec west() :: :west
+  def west, do: :west
 
   @spec get_directions_for(t()) :: %{left: t(), right: t()}
-  def get_directions_for(@east), do: %{@left => @north, @right => @south}
-  def get_directions_for(@north), do: %{@left => @west, @right => @east}
-  def get_directions_for(@south), do: %{@left => @east, @right => @west}
-  def get_directions_for(@west), do: %{@left => @south, @right => @north}
+  def get_directions_for(:east), do: %{@left => :north, @right => :south}
+  def get_directions_for(:north), do: %{@left => :west, @right => :east}
+  def get_directions_for(:south), do: %{@left => :east, @right => :west}
+  def get_directions_for(:west), do: %{@left => :south, @right => :north}
 
   @spec from_string(String.t()) :: t()
-  def from_string("E"), do: @east
-  def from_string("N"), do: @north
-  def from_string("S"), do: @south
-  def from_string("W"), do: @west
+  def from_string("E"), do: east()
+  def from_string("N"), do: north()
+  def from_string("S"), do: south()
+  def from_string("W"), do: west()
 
   @spec to_string(t()) :: String.t()
-  def to_string(@east), do: "E"
-  def to_string(@north), do: "N"
-  def to_string(@south), do: "S"
-  def to_string(@west), do: "W"
+  def to_string(:east), do: "E"
+  def to_string(:north), do: "N"
+  def to_string(:south), do: "S"
+  def to_string(:west), do: "W"
 end
