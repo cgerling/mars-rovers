e3bb2ed011b6466da77b24dce365a3f4bbfddabb
Calvin Gerling <calvin@gerling.sh>
2021-07-10 11:25:02 -0300

    use reporter module to show squad status

--- lib/mars_rovers.ex
+++ lib/mars_rovers.ex
@@ -1,32 +1,14 @@
 defmodule MarsRovers do
   @moduledoc false
 
-  alias MarsRovers.{Commander, Parser, Plateau, Rover}
+  alias MarsRovers.{Commander, Parser, Reporter}
 
   @spec deploy(String.t()) :: String.t()
   def deploy(deploy_instructions) do
     {plateau, squad_with_commands} = Parser.parse_deploy_instructions(deploy_instructions)
 
-    squad = Commander.command_squad(squad_with_commands)
-
-    squad
-    |> Enum.map(&get_rover_status(&1, plateau, squad))
-    |> Enum.join("\n")
-  end
-
-  defp get_rover_status(%Rover{} = rover, %Plateau{} = plateau, squad) when is_list(squad) do
-    status =
-      cond do
-        Rover.is_outside_of?(rover, plateau) ->
-          "out of range"
-
-        Rover.has_crashed_within?(rover, squad) ->
-          "crashed"
-
-        true ->
-          "online"
-      end
-
-    "#{Rover.to_coordinates(rover)} (#{status})"
+    squad_with_commands
+    |> Commander.command_squad()
+    |> Reporter.report_squad_status(plateau)
   end
 end
